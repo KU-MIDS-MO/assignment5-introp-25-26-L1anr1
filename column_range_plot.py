736372273440fe53582369053e93fc852a31bae0
L1anr1 <124812538+L1anr1@users.noreply.github.com>
2026-01-30 15:21:18 +0200

    Update column_range_plot.py

--- column_range_plot.py
+++ column_range_plot.py
@@ -2,6 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def column_range_plot(A, filename="column_ranges.pdf"):
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+
+    max_values = np.max(A, axis=0)
+    min_values = np.min(A, axis=0)
+    
+    ranges = max_values - min_values
+    
+    num_columns = A.shape[1]
+    x_positions = np.arange(num_columns)
+    
+
+    plt.figure()
+    plt.bar(x_positions, ranges)
+    plt.xlabel("Column Index")
+    plt.ylabel("Range")
+    
+    plt.savefig(filename)
+    plt.close()
+    
+    return ranges
